92ce08092515a13c04f815e776a9dab49eaed181
kaitucker <kaituckersample@gmail.com>
2026-03-31 04:36:25 +0000

    Let's play!

--- TicTacToe.py
+++ TicTacToe.py
@@ -207,7 +207,7 @@ def redo_play_game():
             answer = input().strip().upper()
 
             if answer == 'Y':
-                print("Let's play!")
+                print("Let's play Tic-Tac-Toe!")
                 valid_input = True
             elif answer == 'N':
                 play_again = False
